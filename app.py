936eeb39f76d88333cf0e7496f4358ac724c733e
wc18084 <18084@wc.school.nz>
2022-05-20 11:48:04 +1200

    login and edit as teacher

--- app.py
+++ app.py
@@ -21,18 +21,26 @@ def create_connection(db_file):
 def render_homepage():
     con = create_connection(DB_NAME)
 
-    query = "SELECT Category FROM Category"
+    query = "SELECT Category, id FROM Category"
 
     cur = con.cursor()
     cur.execute(query)
     Category_list = cur.fetchall()
     con.close()
 
-    return render_template('home.html', Categorys=Category_list, logged_in=is_logged_in())
+    email = session.get("email")
+    query4 = "SELECT fname, lname, teacher FROM customer WHERE email=?"
+    con4 = create_connection(DB_NAME)
+    cur4 = con4.cursor()
+    cur4.execute(query4, (email,))
+    User_list = cur4.fetchall()
+    con4.close()
+    return render_template('home.html', Categorys=Category_list, Users=User_list, teacher=is_teacher(),
+                           logged_in=is_logged_in())
 
 
-@app.route('/word/<category>')
-def render_wordpage(category):
+@app.route('/word/<category>/<id>')
+def render_wordpage(category, id):
     con = create_connection(DB_NAME)
 
     query = "SELECT Maori, English, Category, Definition, Level, Image, id FROM Dictionary where Category=?"
@@ -44,12 +52,29 @@ def render_wordpage(category):
 
     con2 = create_connection(DB_NAME)
 
-    query2 = "SELECT Category FROM Category"
+    query2 = "SELECT Category, id FROM Category where id=?"
 
     cur2 = con2.cursor()
-    cur2.execute(query2)
-    Category_list = cur2.fetchall()
+    cur2.execute(query2, (id,))
+    id_list = cur2.fetchall()
     con2.close()
+
+    con = create_connection(DB_NAME)
+
+    query = "SELECT Category, id FROM Category"
+
+    cur = con.cursor()
+    cur.execute(query)
+    Category_list = cur.fetchall()
+    con.close()
+
+    email = session.get("email")
+    query4 = "SELECT fname, lname, teacher FROM customer WHERE email=?"
+    con4 = create_connection(DB_NAME)
+    cur4 = con4.cursor()
+    cur4.execute(query4, (email,))
+    User_list = cur4.fetchall()
+    con4.close()
 
     Update_word_list = []
 
@@ -59,7 +84,8 @@ def render_wordpage(category):
             update_word[5] = "noimage"
         Update_word_list.append(update_word)
 
-    return render_template('word.html', Dictionarys=Dictionary_list, Categorys=Category_list, Update_words=Update_word_list, logged_in=is_logged_in(),)
+    return render_template('word.html', Dictionarys=Dictionary_list, ids=id_list, Users=User_list, teacher=is_teacher(),
+                           Categorys=Category_list, Update_words=Update_word_list, logged_in=is_logged_in(), )
 
 
 @app.route('/detail/<maori>/<id>', methods=["GET", "POST"])
@@ -82,6 +108,14 @@ def render_detailpage(maori, id):
     Category_list = cur2.fetchall()
     con2.close()
 
+    email = session.get("email")
+    query4 = "SELECT fname, lname, teacher FROM customer WHERE email=?"
+    con4 = create_connection(DB_NAME)
+    cur4 = con4.cursor()
+    cur4.execute(query4, (email,))
+    User_list = cur4.fetchall()
+    con4.close()
+
     Update_word_list = []
 
     for word in Dictionary_list:
@@ -89,7 +123,6 @@ def render_detailpage(maori, id):
         if update_word[5] is None:
             update_word[5] = "noimage"
         Update_word_list.append(update_word)
-
 
     if request.method == "POST":
         Maori = request.form.get('Maori')
@@ -105,8 +138,8 @@ def render_detailpage(maori, id):
         cur3 = con3.cursor()
 
         Category_list = ["Actions", "Animals", "Clothing", "Culture / Religion", "Descriptive", "Emotions", "Food",
-                        "Math / Number", "Outdoors", "People", "Places", "Plants", "School", "Sport", "Technology",
-                        "Time", "Others"]
+                         "Math / Number", "Outdoors", "People", "Places", "Plants", "School", "Sport", "Technology",
+                         "Time", "Others"]
         re_Category = False
         if Category not in Category_list:
             re_Category = True
@@ -120,7 +153,8 @@ def render_detailpage(maori, id):
         con3.close()
         return redirect("/")
 
-    return render_template('detail.html', Dictionarys=Dictionary_list, Categorys=Category_list, Update_words=Update_word_list, logged_in=is_logged_in())
+    return render_template('detail.html', Dictionarys=Dictionary_list, Categorys=Category_list, Users=User_list,
+                           teacher=is_teacher(), Update_words=Update_word_list, logged_in=is_logged_in())
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -129,27 +163,31 @@ def render_login_page():
         email = request.form.get("email")
         password = request.form.get("password")
 
-        query = """SELECT id, email, password FROM customer"""
+        query = """SELECT id, email, password, teacher FROM customer where email=?"""
         con = create_connection(DB_NAME)
         cur = con.cursor()
-        cur.execute(query)
+        cur.execute(query, (email,))
         user_data = cur.fetchall()
         con.close()
 
         try:
             userid = user_data[0][0]
             db_password = user_data[0][2]
+            teacherid = user_data[0][3]
         except IndexError:
             return redirect("/login?error=Email+invalid+or+password+incorrect")
 
         if db_password != password:
+            print(db_password, password)
             return redirect("/login?error=Email+invalid+or+password+incorrect")
+
 
         session["email"] = email
         session["userid"] = userid
+        session["teacher"] = teacherid
         print(session)
         return redirect("/")
-    return render_template('login.html', logged_in=is_logged_in())
+    return render_template('login.html', teacher=is_teacher(), logged_in=is_logged_in())
 
 
 @app.route('/signup', methods=["GET", "POST"])
@@ -161,24 +199,26 @@ def render_signup_page():
         email = request.form.get('email')
         password = request.form.get("password")
         password2 = request.form.get("password2")
+        teacher = request.form.get("teacher")
 
         if password != password2:
             return redirect("/signup?error=Passwords+dont+match")
 
         con = create_connection(DB_NAME)
 
-        query = "INSERT INTO customer(id, fname, lname, email, password) VALUES(NULL,?,?,?,?)"
+        query = "INSERT INTO customer(id, fname, lname, email, password, teacher) VALUES(NULL,?,?,?,?,? or NULL)"
 
         cur = con.cursor()
         try:
-            cur.execute(query, (fname, lname, email, password))
+            cur.execute(query, (fname, lname, email, password, teacher))
         except sqlite3.IntegrityError:
             return redirect("/signup?error=Email+is+already+used")
 
         con.commit()
         con.close()
+        return redirect("/")
 
-    return render_template('signup.html', logged_in=is_logged_in())
+    return render_template('signup.html', teacher=is_teacher(), logged_in=is_logged_in())
 
 
 @app.route('/logout')
@@ -194,7 +234,7 @@ def delete(maori):
     query = "DELETE FROM Dictionary where Maori=?"
     con = create_connection(DB_NAME)
     cur = con.cursor()
-    cur.execute(query,(maori,))
+    cur.execute(query, (maori,))
 
     print("word deleted")
 
@@ -212,6 +252,15 @@ def is_logged_in():
         return True
 
 
+def is_teacher():
+    if session.get("teacher") is None:
+        print("not teacher")
+        return False
+    else:
+        print("teacher")
+        return True
+
+
 @app.route('/edit', methods=["GET", "POST"])
 def render_edit_page():
     if request.method == "POST":
@@ -221,7 +270,6 @@ def render_edit_page():
         Category = request.form.get('Category')
         Definition = request.form.get("Definition")
         Level = request.form.get("Level")
-
 
         con = create_connection(DB_NAME)
 
@@ -253,6 +301,16 @@ def render_edit_page():
     Category_list = cur2.fetchall()
     con2.close()
 
-    return render_template('edit.html', Categorys=Category_list, logged_in=is_logged_in())
+    email = session.get("email")
+    query4 = "SELECT fname, lname, teacher FROM customer WHERE email=?"
+    con4 = create_connection(DB_NAME)
+    cur4 = con4.cursor()
+    cur4.execute(query4, (email,))
+    User_list = cur4.fetchall()
+    con4.close()
+
+    return render_template('edit.html', Categorys=Category_list, Users=User_list, teacher=is_teacher(),
+                           logged_in=is_logged_in())
+
 
 app.run(host='0.0.0.0', debug=True)
