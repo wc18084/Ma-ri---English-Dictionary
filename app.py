05e2320cf4518c897d9b1bc8faaff924a70fd933
18084 <18084@wc.school.nz>
2022-05-26 11:04:16 +1200

    edit time

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, session, redirect
 import sqlite3
 from sqlite3 import Error
+from datetime import datetime
 
 DB_NAME = "Dictionary.db"
 app = Flask(__name__)
@@ -90,9 +91,41 @@ def render_wordpage(category, id):
 
 @app.route('/detail/<maori>/<id>', methods=["GET", "POST"])
 def render_detailpage(maori, id):
+    if request.method == "POST":
+        print(request.form)
+        Maori = request.form.get('Maori')
+        English = request.form.get('English')
+        Category = request.form.get('Category')
+        Definition = request.form.get("Definition")
+        Level = request.form.get("Level")
+        date = datetime.now()
+        Time = date.strftime('%Y-%m-%d %H:%M:%S.%f')
+
+        con3 = create_connection(DB_NAME)
+
+        query3 = "Update Dictionary SET Maori=?, English=?, Category=?, Definition=?, Level=?, Time=? WHERE id=?"
+
+        cur3 = con3.cursor()
+
+        Category_list = ["Actions", "Animals", "Clothing", "Culture / Religion", "Descriptive", "Emotions", "Food",
+                         "Math / Number", "Outdoors", "People", "Places", "Plants", "School", "Sport", "Technology",
+                         "Time", "Others"]
+        re_Category = False
+        if Category not in Category_list:
+            re_Category = True
+        print(Maori, English, Category, Definition, Level, Time, id)
+        if re_Category is True:
+            return redirect("/edit?error=Category+is+invalid")
+        else:
+            cur3.execute(query3, (Maori, English, Category, Definition, Level, Time, id))
+
+        con3.commit()
+        con3.close()
+        return redirect("/")
+
     con = create_connection(DB_NAME)
 
-    query = "SELECT Maori, English, Category, Definition, Level, Image FROM Dictionary where Maori=?"
+    query = "SELECT Maori, English, Category, Definition, Level, Image, Time FROM Dictionary where Maori=?"
 
     cur = con.cursor()
     cur.execute(query, (maori,))
@@ -101,7 +134,7 @@ def render_detailpage(maori, id):
 
     con2 = create_connection(DB_NAME)
 
-    query2 = "SELECT Category FROM Category"
+    query2 = "SELECT Category, id FROM Category"
 
     cur2 = con2.cursor()
     cur2.execute(query2)
@@ -124,34 +157,7 @@ def render_detailpage(maori, id):
             update_word[5] = "noimage"
         Update_word_list.append(update_word)
 
-    if request.method == "POST":
-        Maori = request.form.get('Maori')
-        English = request.form.get('English')
-        Category = request.form.get('Category')
-        Definition = request.form.get("Definition")
-        Level = request.form.get("Level")
 
-        con3 = create_connection(DB_NAME)
-
-        query3 = "Update Dictionary SET Maori=?, English=?, Category=?, Definition=?, Level=?, Image=Null WHERE id=?"
-
-        cur3 = con3.cursor()
-
-        Category_list = ["Actions", "Animals", "Clothing", "Culture / Religion", "Descriptive", "Emotions", "Food",
-                         "Math / Number", "Outdoors", "People", "Places", "Plants", "School", "Sport", "Technology",
-                         "Time", "Others"]
-        re_Category = False
-        if Category not in Category_list:
-            re_Category = True
-        print(Maori, English, Category, Definition, Level, id)
-        if re_Category is True:
-            return redirect("/edit?error=Category+is+invalid")
-        else:
-            cur3.execute(query3, (Maori, English, Category, Definition, Level, id))
-
-        con3.commit()
-        con3.close()
-        return redirect("/")
 
     return render_template('detail.html', Dictionarys=Dictionary_list, Categorys=Category_list, Users=User_list,
                            teacher=is_teacher(), Update_words=Update_word_list, logged_in=is_logged_in())
@@ -270,15 +276,16 @@ def render_edit_page():
         Category = request.form.get('Category')
         Definition = request.form.get("Definition")
         Level = request.form.get("Level")
+        Time = datetime.now()
 
         con = create_connection(DB_NAME)
 
-        query = "INSERT INTO Dictionary(Maori, English, Category, Definition, Level, Image) VALUES(?,?,?,?,?,Null)"
+        query = "INSERT INTO Dictionary(Maori, English, Category, Definition, Level, Image, Time) VALUES(?,?,?,?,?,Null,?)"
 
         cur = con.cursor()
 
-        Category_list = ["Actions", "Animals", "Clothing", "Culture / Religion", "Descriptive", "Emotions", "Food",
-                         "Math / Number", "Outdoors", "People", "Places", "Plants", "School", "Sport", "Technology",
+        Category_list = ["Actions", "Animals", "Clothing", "Culture and Religion", "Descriptive", "Emotions", "Food",
+                         "Math and Number", "Outdoors", "People", "Places", "Plants", "School", "Sport", "Technology",
                          "Time", "Others"]
         re_Category = False
         if Category not in Category_list:
@@ -287,14 +294,15 @@ def render_edit_page():
         if re_Category is True:
             return redirect("/edit?error=Category+is+invalid")
         else:
-            cur.execute(query, (Maori, English, Category, Definition, Level))
+            cur.execute(query, (Maori, English, Category, Definition, Level, Time))
+
 
         con.commit()
         con.close()
 
     con2 = create_connection(DB_NAME)
 
-    query2 = "SELECT Category FROM Category"
+    query2 = "SELECT Category, id FROM Category"
 
     cur2 = con2.cursor()
     cur2.execute(query2)
